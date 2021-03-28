9c1879b1d392ea3b16a417d74d91f158fd67493e
Vladislav <vlad_moryakov_prog@mail.ru>
2021-03-28 10:57:36 +0300

    Добавил адекватную клавиатуру, теперь под ней появляется нав бар и ничего не ломается и не дергается

--- app/src/main/java/com/example/samsungnormalannualproject/SignUpActivity.java
+++ app/src/main/java/com/example/samsungnormalannualproject/SignUpActivity.java
@@ -2,40 +2,59 @@ package com.example.samsungnormalannualproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private View view;
+    private Button confirmButton;
+    private EditText nickNameEditText;
+    private EditText passwordEditText;
+    private EditText passwordAgainEditText;
 
-    // скрывает боттом меню
+    // скрывает боттом меню при потере фокуса
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            hideSystemUI();
         }
     }
 
     // скрывает топ меню и делаем приложения безрамочным
     public void hideSystemUI() {
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_LOW_PROFILE
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        );
+    }
+
+    public void showBottomNavBar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_VISIBLE // именно это поле и меняет состояние нижнего меню
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
     }
 
     private void setBackGround() {
@@ -45,17 +64,18 @@ public class SignUpActivity extends AppCompatActivity {
 
         if (isMen) {
             this.view.setBackgroundResource(R.drawable.select_gender_gradient_boy);
+            this.confirmButton.setBackgroundResource(R.drawable.login_regirstrate_button_background_boy);
         }
 
         if (!isMen) {
             this.view.setBackgroundResource(R.drawable.select_gender_gradient_girl);
+            this.confirmButton.setBackgroundResource(R.drawable.login_registrate_button_background_girl);
         }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
 
         // сдесь у нас настройка активити берзрамочноая
@@ -71,7 +91,31 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         this.view = (View) findViewById(R.id.sign_up_view);
+        this.confirmButton = (Button) findViewById(R.id.confirm_sign_up_button);
+        this.nickNameEditText = (EditText) findViewById(R.id.editText_password);
+        this.passwordEditText = (EditText) findViewById(R.id.editText_password);
+        this.passwordAgainEditText = (EditText) findViewById(R.id.editText_password_again);
 
-        setBackGround();
+        this.setBackGround();
+        this.keyBoardStateChangeListener();
     }
+
+    private void keyBoardStateChangeListener() {
+        this.view.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        Rect r = new Rect();
+                        view.getWindowVisibleDisplayFrame(r);
+                        int screenHeight = view.getRootView().getHeight();
+                        int keypadHeight = screenHeight - r.bottom;
+                        if (keypadHeight > screenHeight * 0.15) {
+                            showBottomNavBar();
+                        } else {
+                            hideSystemUI();
+                        }
+                    }
+                });
+    }
+
 }

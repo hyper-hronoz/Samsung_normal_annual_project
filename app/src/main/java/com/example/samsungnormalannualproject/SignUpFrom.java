e142c55730aefdecb6dfba6a2388ba540aef5565
Vladislav <vlad_moryakov_prog@mail.ru>
2021-04-03 17:18:14 +0300

    почистил код

--- app/src/main/java/com/example/samsungnormalannualproject/SignUpFrom.java
+++ app/src/main/java/com/example/samsungnormalannualproject/SignUpFrom.java
@@ -32,13 +32,6 @@ public class SignUpFrom extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // сдесь у нас настройка активити берзрамочноая
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-        }
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
 
         setContentView(R.layout.acitibity_sign_up_fill_form);
 
@@ -46,21 +39,5 @@ public class SignUpFrom extends BaseActivity {
         this.view = findViewById(R.id.sign_up_fill_form_view);
 
         setBackGround();
-
-        hideSystemUI();
-    }
-
-    @Override
-    // скрывает топ меню и делаем приложения безрамочным
-    public void hideSystemUI() {
-
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }

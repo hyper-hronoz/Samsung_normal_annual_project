15236b8b1e1993a638a5e10bd1d9627d7f8da08e
Vladislav <vlad_moryakov_prog@mail.ru>
2021-04-03 18:41:33 +0300

    почистил код

--- app/src/main/java/com/example/samsungnormalannualproject/BaseActivity.java
+++ app/src/main/java/com/example/samsungnormalannualproject/BaseActivity.java
@@ -9,11 +9,13 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.example.samsungnormalannualproject.interfaces.ActivitySettings;
 
 public class BaseActivity extends AppCompatActivity implements ActivitySettings {
     // паказывает нижнее меню когда приложение открыто
+    private TextView activityHeading;
 
     @Override
     public void keyBoardStateChangeListener() {
@@ -44,6 +46,11 @@ public class BaseActivity extends AppCompatActivity implements ActivitySettings 
 
     @Override
     public void showBottomNavBar() {
+        try {
+            activityHeading.setVisibility(View.VISIBLE);
+        } catch (Exception e) {
+
+        }
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -58,16 +65,22 @@ public class BaseActivity extends AppCompatActivity implements ActivitySettings 
 
     @Override
     public void hideSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        );
+        try {
+            activityHeading.setVisibility(View.GONE);
+        } catch (Exception e) {
+
+        }
+
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_IMMERSIVE
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//        );
     }
 
     @Override
@@ -77,11 +90,16 @@ public class BaseActivity extends AppCompatActivity implements ActivitySettings 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         }
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        try {
+            this.activityHeading = findViewById(R.id.activity_heading);
+        } catch (Exception e) {
+
+        }
         hideSystemUI();
         keyBoardStateChangeListener();
     }
--- app/src/main/java/com/example/samsungnormalannualproject/SignUpActivity.java
+++ app/src/main/java/com/example/samsungnormalannualproject/SignUpActivity.java
@@ -25,7 +25,7 @@ import com.example.samsungnormalannualproject.interfaces.ActivitySettings;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SignUpActivity extends AppCompatActivity implements ActivitySettings {
+public class SignUpActivity extends BaseActivity {
 
     private View view;
     private Button confirmButton;
@@ -38,74 +38,13 @@ public class SignUpActivity extends AppCompatActivity implements ActivitySetting
     private String nickName;
     private String password;
 
-    // скрывает боттом меню при потере фокуса
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideSystemUI();
-        }
-    }
-
-    // скрывает топ меню и делаем приложения безрамочным
-    @Override
-    public void hideSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        );
-    }
-
-    // паказывает нижнее меню когда приложение открыто
-    public void showBottomNavBar() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_VISIBLE // именно это поле и меняет состояние нижнего меню
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        );
-    }
-
-    // устанавливает фон для элементов в зависимости от выбранного пола
-    private void setBackGround() {
-
-        System.out.println(this.view);
-
-        if (this.isMen) {
-            this.view.setBackgroundResource(R.drawable.select_gender_gradient_boy);
-            this.confirmButton.setBackgroundResource(R.drawable.login_regirstrate_button_background_boy);
-        }
-
-        if (!this.isMen) {
-            this.view.setBackgroundResource(R.drawable.select_gender_gradient_girl);
-            this.confirmButton.setBackgroundResource(R.drawable.login_registrate_button_background_girl);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-        }
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_sign_up);
 
-        hideSystemUI();
 
         // ну здесь мы присваем значения для полей
         this.view = (View) findViewById(R.id.sign_up_view);
@@ -116,8 +55,6 @@ public class SignUpActivity extends AppCompatActivity implements ActivitySetting
 
         this.isMen = getIntent().getExtras().getBoolean(SelectGenderActivity.GENDER);
 
-        this.setBackGround();
-        this.keyBoardStateChangeListener();
 
         this.confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -180,32 +117,4 @@ public class SignUpActivity extends AppCompatActivity implements ActivitySetting
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
     }
-
-
-    public interface PostCommentResponseListener {
-        public void requestStarted();
-        public void requestCompleted();
-        public void requestEndedWithError(VolleyError error);
-    }
-
-    // слушаем открыта ли клава или нет
-    @Override
-    public void keyBoardStateChangeListener() {
-        getWindow().getDecorView().getViewTreeObserver().addOnGlobalLayoutListener(
-                new ViewTreeObserver.OnGlobalLayoutListener() {
-                    @Override
-                    public void onGlobalLayout() {
-                        Rect r = new Rect();
-                        getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
-                        int screenHeight = getWindow().getDecorView().getRootView().getHeight();
-                        int keypadHeight = screenHeight - r.bottom;
-                        if (keypadHeight > screenHeight * 0.15) {
-                            showBottomNavBar();
-                        } else {
-                            hideSystemUI();
-                        }
-                    }
-                });
-    }
-
 }

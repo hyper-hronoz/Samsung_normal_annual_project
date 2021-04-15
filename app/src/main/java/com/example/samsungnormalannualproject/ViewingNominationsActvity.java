696d8c12e1f4faa9a0658f6f5b3860823ed63273
Vladislav <vlad_moryakov_prog@mail.ru>
2021-04-15 22:31:04 +0300

    сделал фронт для кандидатов, осталось вынести bottom меню в качестве компонента.

---
+++ app/src/main/java/com/example/samsungnormalannualproject/ViewingNominationsActvity.java
@@ -0,0 +1,14 @@
+package com.example.samsungnormalannualproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewingNominationsActvity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewing_nominations_actvity);
+    }
+}
--- app/src/main/java/com/example/samsungnormalannualproject/WhatKindOfPeopleLookinFor.java
+++ app/src/main/java/com/example/samsungnormalannualproject/WhatKindOfPeopleLookinFor.java
@@ -2,13 +2,27 @@ package com.example.samsungnormalannualproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WhatKindOfPeopleLookinFor extends AppCompatActivity {
+    private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_what_kind_of_people_lookin_for);
+
+        this.confirmButton = findViewById(R.id.confirm_button);
+
+        this.confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ViewingNominationsActvity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
